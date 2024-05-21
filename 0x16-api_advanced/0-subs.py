5eea69196d2bc5036dd9c903872bddee96aa6e69
DLAX DITAU <ditaumolatedi@gmail.com>
2024-05-21 09:20:03 +0200

    fix_api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "alx api project by Mditau"
+        "User-Agent": "Custom User Agent"
     }
     response = requests.get(url , headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
         data = response.json()
         subscribers = data.get('data').get('subscribers')
         return subscribers
